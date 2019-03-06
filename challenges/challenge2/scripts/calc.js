19608136af797ea7bca7085bf81a252d89c55a86
Ivan Toulou <touloumbadjiani@gmail.com>
2019-03-06 11:16:26 +0000

    tests were failing, needed to change instance name

--- challenges/challenge2/scripts/calc.js
+++ challenges/challenge2/scripts/calc.js
@@ -1,11 +1,11 @@
 fizzBuzz = function(number) {
-    if(number % 3 == 0 && number % 5 == 0) {
+    if(number % 3 === 0 && number % 5 === 0) {
         return "FizzBuzz";
-    } else if(number % 5 == 0) {
+    } else if(number % 5 === 0) {
         return "Buzz";
-    } else if(number % 3 == 0) {
+    } else if(number % 3 === 0) {
         return "Fizz";
     } else {
         return number;
     }
-}
+};
--- challenges/challenge2/spec/calcSpec.js
+++ challenges/challenge2/spec/calcSpec.js
@@ -1,17 +1,21 @@
 describe("FizzBuzz", function() {
     beforeEach(function() {
-       fizzBuzz = new fizzBuzz(); 
+       fizzbuzz = new fizzBuzz(); 
     });
-    it("should return number", function() {
-       expect(fizzBuzz(1)).toBe(1); 
+    describe("Returns number, fizz, buzz or FizzBuzz", function() {
+        it("should return number", function() {
+            var result = fizzBuzz(1);
+            expect(result).toBe(1); 
+        });
+        it("should return Fizz", function() {
+            expect(fizzBuzz(3)).toBe("Fizz");
+        });
+        it("should return Buzz", function() {
+           expect(fizzBuzz(5)).toBe("Buzz"); 
+        });
+        it("should return FizzBuzz", function() {
+           expect(fizzBuzz(15)).toBe("FizzBuzz") 
+        });
     });
-    it("should return Fizz", function() {
-        expect(fizzBuzz(3)).toBe("Fizz");
-    });
-    it("should return Buzz", function() {
-       expect(fizzBuzz(5)).toBe("Buzz"); 
-    });
-    it("should return FizzBuzz", function() {
-       expect(fizzBuzz(15)).toBe("FizzBuzz") 
-    });
+
 });
